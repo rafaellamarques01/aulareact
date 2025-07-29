02db73e45d7c284e782aff9fc0f33a759776823e
rafaellamarques01 <rafaellamarques979@gmail.com>
2025-07-29 12:25:14 -0300

    29/07

--- estudocss/src/App.jsx
+++ estudocss/src/App.jsx
@@ -1,9 +1,33 @@
+import "./App.css";
+
+
 export default function App()
 {
+   const estilo = {
+    color : "red",
+   textAlign : "center"
+   };
+
     return (
-        <div>
+        <div className="conteudo">
             <h1>Estudo CSS</h1>
             
+            <p>
+                CSS (<i>Cascading Style Sheets</i>) é uma linguagem utilizada para estilizar as <b>páginas web</b>. Com ela podemos definir:
+            </p>
+
+            <ul>
+                <li>cores de fundo, do texto e dos elementos.</li>
+                <li>Fontes e tamanho das letras </li>
+                <li>Posicionamento dos elementos na pagina</li>
+                 <li>Espaçamento, bordas, sombras, etc. </li>
+                 <li>Efeitos visuais e animações</li>
+            </ul>
+                  <h3> style={estilo}Cidade De Mococa</h3>
+
+              <p className="centro">
+                <img src="mococa.jpg" alt=""  />
+              </p>
         </div>
     );
 }
