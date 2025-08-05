97d295de3fe133209b048ab454f6b33bb49be36e
rafaellamarques01 <rafaellamarques979@gmail.com>
2025-08-05 12:22:35 -0300

    05/08

---
+++ estudorotas/src/Paginas/Home.jsx
@@ -0,0 +1,25 @@
+export  default function Home()
+{
+    return (
+        <div>
+            <h1>Estudo de Rotas</h1>
+
+            <h3>Estudos da Região Sudeste</h3>
+            <ul>
+                <li><a href="/sp">São Paulo</a></li>
+            </ul>
+
+            <ul>
+                <li><a href="/rj">Rio de Janeiro</a></li>
+            </ul>
+
+            <ul>
+                <li><a href="/mg">Minas Gerais</a></li>
+            </ul>
+
+            <ul>
+                <li><a href="/es">Espirito Santo</a></li>
+            </ul>
+        </div>
+    )
+}
---
+++ estudorotas/src/Paginas/RioJaneiro.jsx
@@ -0,0 +1,21 @@
+export default function RioJaneiro()
+{  
+return (
+    <div>
+    <h1> Seja bem vindo ao Rio de Janeiro</h1>
+
+    <p>
+    O Rio de Janeiro é famoso por suas paisagens deslumbrantes, como o Cristo Redentor e as praias de Copacabana e Ipanema. A cidade é um importante destino turístico, conhecida pelo Carnaval vibrante e pela música samba. Além disso, o Rio combina natureza exuberante com uma vida urbana intensa e cheia de energia.
+    </p>
+     
+     <p>
+        <img src=""   />
+     </p>
+
+     <p>
+        <li><a href="/">Voltar para Home</a></li>
+     </p>
+
+    </div>
+)
+}
