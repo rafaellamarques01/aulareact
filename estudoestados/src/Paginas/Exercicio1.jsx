e6ebc81732adb30bc4422900182dcff7d73f48b1
rafaellamarques01 <rafaellamarques979@gmail.com>
2025-10-07 12:25:46 -0300

    07/10

--- estudoestados/src/Paginas/Exercicio1.jsx
+++ estudoestados/src/Paginas/Exercicio1.jsx
@@ -1,4 +1,24 @@
-export default function Exercicio1() {
+import { useState } from "react";
+
+export default function Exercicio1() 
+{
+    //variavel de estado para os campos do formulário
+    const[numero, setNumero] = useState(0);
+
+    //variavel de estado para o resultado
+    const[resultado, setResultado] = useState(0);
+
+    //função para calcular o quadrado ou o cubo 
+    function calcular()
+    {
+      let quadrado, cubo;
+
+      quadrado = Number(numero) * Number(numero);
+      cubo = Number(numero) * Number(numero) * Number(numero);
+
+      setResultado("quadrado: " + quadrado + " - cubo:" + cubo);
+    }
+
 return(
     <div>
         <h1>Exercicio 1</h1>
@@ -7,12 +27,21 @@ return(
     <form>
         <p>
             digite um numero qualquer <br />
-            <input type="text" />
+            <input type="text" value={numero}
+           onChange={(e) => setNumero(e.target.value)} />
+           
         </p>
        
        <p>
-        <input type="button" value="Exercicio1" />
+        <input type="button" value="Exercicio1" onClick={calcular} />
        </p>
+
+        <p>
+            <b>Resultado</b>
+            <br   />
+            número é {numero} <br  />
+            resultado é {resultado}
+      </p>
 
        <p> 
         <a href="/">Voltar</a>
--- estudoestados/src/Paginas/Exercicio2.jsx
+++ estudoestados/src/Paginas/Exercicio2.jsx
@@ -1,17 +1,43 @@
-export default function Exercicio2(){
+export default function Exercicio2()
+{
+    
+    const[numero, setNumero] = useState(0);
+    const[resultado, setResultado] = useState(0);
+
+    
+    function calcular()
+    {
+        let celsius;
+
+        celsius = ((Number(numero) - 32) * 5)/9;
+
+        setResultado("graus celsius: " + celsius);
+
+    }
+
     return(
         <div>
             <h1>Exercicio 2</h1>
             <div classname="conteudo"></div>
-        
+
+        <h3>calculo da temperatura em graus Fahrenheit:</h3>
+
             <form>
                 <p>
                     digite a temperatura em graus Fahrenheit:<br  />
-                    <input type="text"  />
+                    <input type="text"  value={numero}
+                    onChange={(e)=> setNumero(e.target.value)}/>
                 </p>
 
                 <p>
-                    <input type="button" value="Execicio2"  />
+                    <input type="button" value="Execicio2"  onClick ={calcular}/>
+                </p>
+
+                <p>
+                    <b>resultado</b>
+                    <br  />
+                    fehrenheit é {numero} <br />
+                    resultado em celsius é {resultado}
                 </p>
                 <p>
                     <a href="/">Voltar</a>
